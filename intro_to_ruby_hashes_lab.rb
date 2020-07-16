6695bad9b810b5fd45505d7eb3abbb7c69b87c85
Antonio James <asjames18@gmail.com>
2020-07-16 16:46:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,8 +31,11 @@ end
 def monopoly_with_third_tier
 #
 monopoly_with_third_tier = {
-  :rent_in_dollars => 3,
-   :one_piece_owned => 25
+  :rent_in_dollars => 25,
+   :one_piece_owned => 3
+ }
+ :rent_in_dollars => 50
+ :two_pieces_owned => 2
 
 }
 end
