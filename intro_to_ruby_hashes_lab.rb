dd1d292e26e1fad51b7b9d49c7611eaaf564fc8b
Antonio James <asjames18@gmail.com>
2020-07-16 16:01:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ end
 
 def monopoly_with_second_tier
 monopoly_with_second_tier = {
-  :railroads => { pieces => {}, :names => {}, :rent_in_dollars => {}
+  :railroads => { :pieces => {}, :names => {}, :rent_in_dollars => {}
 }
 }
 
